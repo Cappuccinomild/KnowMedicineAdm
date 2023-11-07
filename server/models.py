14a25ea755eb4358b43b2dade7666488383ede33
Cappuccinomild <rkdquddh1208@naver.com>
2023-11-07 13:09:07 +0900

    .

--- server/models.py
+++ server/models.py
@@ -23,11 +23,20 @@ class User(db.Model):
     gender = db.Column(db.String(2), nullable=True, comment = "유저 성별")
     phone = db.Column(db.String(17), nullable=True, comment = "유저 전화번호")
 
+    def id_validation(id, pwd=None):
+
+        # pwd가 주어지지 않았을 때
+        if pwd is None:
+            user = User.query.filter(User.user_id == id).all()
+        else:
+            user = User.query.filter(User.user_id == id, User.password == pwd).all()
+
+        return len(user)
+
 class User_log(db.Model):
     user_log_id = db.Column(db.String(12), primary_key=True, comment = "사진 전송 로그 ID")
     user_id = db.Column(db.String(12), nullable=False, comment = "유저 ID")
     img_id = db.Column(db.String(12), nullable=False, comment = "이미지 ID")
-    rate = db.Column(db.Integer, nullable=False, comment = "분석 결과 평균 정확도")
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, comment = "전송날짜")
 
 class Check_log(db.Model):
@@ -47,18 +56,95 @@ class ID_seq(db.Model):
     ID = db.Column(db.String(3), primary_key=True, comment = "키 앞자리 세자리")
     seq = db.Column(db.Integer, nullable=False, comment = "키 번호")
 
+    @staticmethod
+    def call_ID(id):
+        try:
+            # 하나의 결과만 예상한다면 all() 대신 first()를 사용합니다.
+            query = ID_seq.query.filter(ID_seq.ID == id).first()
+
+            if query:
+                return_seq = str(query.seq).zfill(8)
+                query.seq += 1
+
+                db.session.commit()
+
+                return f"{id}_{return_seq}"
+
+            else:
+                return None
+        except Exception as e:
+            # 데이터베이스 작업 중 예외를 처리합니다. 예를 들어 오류를 기록합니다.
+            print(f"오류: {e}")
+            db.session.rollback()  # 오류 발생 시 변경 사항을 롤백합니다.
+            return None
+
 class Img_set(db.Model):
     img_id = db.Column(db.String(12), primary_key=True, comment = "이미지 ID")
     user_id = db.Column(db.String(12), nullable=True, comment = "이미지 출처 ID")
-    img_dir = db.Column(db.String(40), nullable=False, comment = "이미지 저장 장소")
+    img_dir = db.Column(db.Text, nullable=False, comment = "이미지 저장 장소")
     train_cnt = db.Column(db.Integer, nullable=False, default=0, comment = "학습에 사용된 횟수")
+    rate = db.Column(db.Float, nullable=False, comment = "분석 결과 평균 정확도")
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, comment = "저장된 날짜")
+
+    def add_img(img_id, user_id, img_dir, rate):
+        """
+        Add image data to the database.
+
+        :param img_id: Image ID.
+        :param user_id: User ID.
+        :param img_dir: Image directory.
+        :param train_cnt: Number of times the image was used for training.
+        """
+        if not all([img_id, user_id, img_dir]):
+            print([img_id, user_id, img_dir])
+            raise ValueError("Image data should contain img_id, user_id, img_dir, and rate as valid inputs.")
+
+        new_img = Img_set(
+            img_id=img_id,
+            user_id=user_id,
+            img_dir=img_dir,
+            train_cnt=0,
+            rate=rate,
+            date=datetime.utcnow()
+        )
+
+        db.session.add(new_img)
+        db.session.commit()
 
 class Tag_set(db.Model):
     tag_id = db.Column(db.String(12), primary_key=True, comment = "태그 ID")
     img_id = db.Column(db.String(12), nullable=False, comment = "이미지 ID")
     med_id = db.Column(db.String(12), nullable=False, comment = "태깅 의약품 ID")
+    rate = db.Column(db.Float, nullable=False, comment = "분석 정확도")
     start_x = db.Column(db.Float, nullable=True, comment = "좌표 x1")
     start_y = db.Column(db.Float, nullable=True, comment = "좌표 y1")
     end_x = db.Column(db.Float, nullable=True, comment = "좌표 x2")
     end_y = db.Column(db.Float, nullable=True, comment = "좌표 y2")
+
+    def add_tag(tag_id, img_id, med_id, rate, start_x=None, start_y=None, end_x=None, end_y=None):
+        """
+        Add tag data to the database.
+
+        :param img_id: Image ID.
+        :param med_id: Tagged medicine ID.
+        :param start_x: Starting x-coordinate (optional).
+        :param start_y: Starting y-coordinate (optional).
+        :param end_x: Ending x-coordinate (optional).
+        :param end_y: Ending y-coordinate (optional).
+        """
+        if not all([img_id, med_id]):
+            raise ValueError("Image ID and medicine ID are required for tagging.")
+
+        new_tag = Tag_set(
+            tag_id = tag_id,
+            img_id = img_id,
+            med_id = med_id,
+            rate = rate,
+            start_x = start_x,
+            start_y = start_y,
+            end_x = end_x,
+            end_y = end_y
+        )
+
+        db.session.add(new_tag)
+        db.session.commit()
--- server/views/api_views.py
+++ server/views/api_views.py
@@ -24,29 +24,76 @@ def PIL2OpenCV(pil_image):
     
     return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-def yolo_img_predict(img):
+def yolo_img_predict(id, img):
     
     yolo_model = YOLO("../img_model.pt")
 
     results = yolo_model(img)
 
+    from server.models import ID_seq, Img_set, Tag_set
+    
+    image_data = {}
+    
     for r in results:
 
         # show result in server
         im_array = r.plot()  # plot a BGR numpy array of predictions
         im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
         im.show()  # show image
+        
+        image_data['img_id'] = ID_seq.call_ID('IMG')
+
+        path_month = datetime.datetime.now().strftime('%Y%m')
+        fname_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+
+        # 이미지 저장위치 생성
+        path = './image/' + path_month
+        
+        fname = "_".join([image_data['img_id'], fname_time])
+        fname += ".png"
+
+        # 이미지 저장
+        os.makedirs(path,exist_ok=True)
+        cv2.imwrite(path + "/" + fname, img)
+
+        image_data['user_id'] = id
+        image_data['img_dir'] = path + "/" + fname
 
         correct_rate = r.boxes.conf
         label = r.boxes.cls
 
-        print(r.boxes)
+        bbox_xyxy = r.boxes.xyxy.tolist()
+        
+        # 이미지의 평균 정확도 계산
+        image_data['rate'] = np.mean(np.array(correct_rate))
+
+        # 이미지 입력
+        print(image_data)
+        Img_set.add_img(**image_data)
 
     rate_list = []
     label_list = []
 
     low_accuracy = []
-    for rate, label in zip(correct_rate, label):
+
+    
+
+    for rate, label, xyxy in zip(correct_rate, label, bbox_xyxy):
+        
+        start_x, start_y, end_x, end_y = xyxy
+        # init tag data
+        tag_data = {}
+
+        tag_data['tag_id'] = ID_seq.call_ID('TAG')
+        tag_data['img_id'] = image_data['img_id']
+        tag_data['rate'] = rate
+        tag_data['start_x'] = start_x
+        tag_data['start_y'] = start_y
+        tag_data['end_x'] = end_x
+        tag_data['end_y'] = end_y
+
+        print(tag_data)
+        Tag_set.add_tag(**tag_data)
 
         if rate >= 0.8:
             rate_list.append(rate)
@@ -65,19 +112,6 @@ def get_medicine_data(label):
         "medicine_list" : []
     }
 
-    # 1개의 객체만 들어올 경우
-    # prev (8,)
-    # mid ('8',) 이 부분의 콤마를 제거함
-    # now ('8')
-    # label을 파싱하여 리스트로 변환
-    
-    print(label)
-    '''
-    label = str(tuple(map(str, label)))
-    print(label)
-    label = label.replace(",)", ")")
-    print(label)
-    '''
     from server.models import Medicine
     # SQLAlchemy ORM을 사용한 쿼리
     result = Medicine.query.filter(Medicine.class_id.in_(label)).all()
@@ -91,7 +125,6 @@ def get_medicine_data(label):
 
     json_skeleton['medicine_list'] = medicine_list
 
-    print(json_skeleton)
     return json_skeleton
 
 def save_nonelabel(id, img, low_accuracy, res_img):
@@ -116,14 +149,6 @@ def save_nonelabel(id, img, low_accuracy, res_img):
     cv2.imwrite(path + "/" + fname, img)
     cv2.imwrite(res_path + "/" + fname, res_img)
 
-def id_validation(id, pwd):
-
-    from server.models import User
-    
-    user = User.query.filter(User.user_id==id, User.password == pwd).all()
-
-    return len(user)
-
 from flask import Blueprint, render_template
 
 bp = Blueprint('api', __name__, url_prefix='/')
@@ -142,8 +167,9 @@ def ID_json_handler():
         user_pw = content['password']
         
         print(content)
+        from server.models import User
         # user id와 pw 검증
-        validation_flag = id_validation(user_id, user_pw)
+        validation_flag = User.id_validation(user_id, user_pw)
         
         # id와 pwd가 일치하는 결과가 1개 뿐일때.
         if validation_flag == 1:
@@ -191,7 +217,7 @@ def photo_json_handler():
     # photo.show()
     photo = PIL2OpenCV(photo)
         
-    correct_rate, label, low_accuracy, res_img = yolo_img_predict(photo)
+    correct_rate, label, low_accuracy, res_img = yolo_img_predict(user_id, photo)
     
     # 검출 결과 이미지 cv2 파일로 변환
     res_img = PIL2OpenCV(res_img)
@@ -229,7 +255,7 @@ def signup_json_handler():
 
         from server.models import User
     
-        validation_flag = len(User.query.filter(User.user_id==id).all())
+        validation_flag = User.id_validation(user_id)
 
         # 이미 존재하는 회원 ID
         if validation_flag == 0:
@@ -266,7 +292,7 @@ def is_valid_id():
         
         from server.models import User
     
-        validation_flag = len(User.query.filter(User.user_id==user_id).all())
+        validation_flag = User.id_validation(user_id)
 
         # 존재하지 않는 ID
         if validation_flag == 0:
